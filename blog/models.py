b044bc4f1ba02711b6126d8f5a118e33dc666382
XiaZhengXin <xzx@xiazhengxin.name>
2017-10-12 17:00:03 +0800

    basic model.
    Signed-off-by: XiaZhengXin <xzx@xiazhengxin.name>

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
+
 # Create your models here.
+class Topic(models.Model):
+    title = models.TextField(max_length=100)
+    content = models.TextField(max_length=100000)
+    publish_time = models.DateTimeField(auto_now_add=True)
+    tag = models.TextField(max_length=100)
+
+
+class Comment(models.Model):
+    tid = models.ForeignKey(to=Topic)
+    username = models.TextField(max_length=100)
+    email = models.TextField(max_length=100)
+    content = models.TextField(max_length=1000)
+    publish_time = models.DateTimeField(auto_now_add=True)
+
+
+class Variable(models.Model):
+    keyName = models.TextField(max_length=30)
+    keyValue = models.TextField(max_length=50)
--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,26 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Topic, Comment, Variable
+from django.forms.models import model_to_dict
 
 
 # Create your views here.
-
 def index(request):
     return HttpResponse("OK")
+
+
+def get_variable_by_key(key):
+    obj = None
+    try:
+        obj = Variable.objects.get(keyName=key)
+    except Variable.DoesNotExist:
+        obj = None
+    if obj is None:
+        return None
+    else:
+        dict_obj = model_to_dict(obj)
+        value = dict_obj['keyValue']
+        if value.strip() == '':
+            return None
+        else:
+            return value
